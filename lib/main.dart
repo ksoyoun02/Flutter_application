7cba6541ea38083b4597efb3ecf926d97fec6b8f
ksoyoun02 <ksoyoun95@naver.com>
2024-11-15 17:09:19 +0900

    edit

--- lib/main.dart
+++ lib/main.dart
@@ -4,34 +4,66 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  var openYN = true;
+
+  void btnClick() {
+    setState(() {
+      openYN = !openYN;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-          appBar: AppBar(
-            centerTitle: true,
-            title: const Text("test"),
-            backgroundColor: const Color.fromARGB(255, 235, 238, 243),
-          ),
-          body: Column(
-            children: [
-              Flexible(
-                flex: 2,
-                child: Container(
-                  color: Colors.red,
-                ),
+        appBar: AppBar(
+          centerTitle: true,
+          title: const Text("test"),
+          backgroundColor: const Color.fromARGB(255, 235, 238, 243),
+        ),
+        body: Column(
+          children: [
+            // 첫 번째 컨테이너
+            AnimatedContainer(
+              duration: const Duration(milliseconds: 300),
+              height: openYN ? 200 : 60, // 크기 조정
+              color: Colors.red,
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      IconButton(
+                        onPressed: btnClick,
+                        icon: Icon(
+                          openYN
+                              ? Icons.keyboard_arrow_up_sharp
+                              : Icons.keyboard_arrow_down_sharp,
+                          size: 40,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
               ),
-              Flexible(
-                flex: 5,
-                child: Container(
-                  color: Colors.yellow,
-                ),
-              )
-            ],
-          )),
+            ),
+            // 두 번째 컨테이너
+            Expanded(
+              child: Container(
+                color: Colors.yellow,
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
